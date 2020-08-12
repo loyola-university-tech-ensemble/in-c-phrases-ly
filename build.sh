22873310ca7d632f9a8bf6b27aeefc4dbbd37ae7
Griffin Moe <me@griffinmoe.com>
2020-08-12 07:04:26 -0500

    v1.0.0 release
    The generated SVGs and Lilypond source files will now be available
    as an NPM package hosted through GitHub. This package will be used in
    other LUTE applications such as the In C score progress viewer and the
    in-development web app.
    
    The SVGs are also pre-optimized using SVGO.
    
    Part of me wants to rewrite the build script as a Node script but
    replacing the file I/O was going to result in way more code than the
    current build script with no real advantages, so I'm leaving it as-is
    for now.

--- build.sh
+++ build.sh
@@ -1,41 +1,41 @@
+#!/bin/bash
 IN_DIR='./src/'
-OUT_DIR='./build/'
+OUT_DIR='./svgs/'
 
-# Check for presence of Lilypond
 if [ ! -x "$(command -v lilypond)" ]; then
   echo 'Lilypond is not installed. It can be found at http://lilypond.org/' >&2
   exit 1
 fi
 
-# Make output directory?
 if [ ! -d $OUT_DIR ]; then
   mkdir -p $OUT_DIR
 fi
 
-function make_svg {
-  lilypond --loglevel=BASIC_PROGRESS --output=$OUT_DIR -dpoint-and-click='#f' -dno-print-pages -dpreview -dbackend=svg $1
+npm install
+
+make_svg() {
+  lilypond --loglevel=BASIC_PROGRESS --output="$OUT_DIR" -dpoint-and-click='#f' -dno-print-pages -dpreview -dbackend=svg "$1"
 }
 
-function prep_file {
-  echo "<?xml-stylesheet type='text/css' href='/styles.css' ?>\n$(cat $1)" > $1
-  mv $1 "`echo $1 | sed 's/preview\.//'`"
+prep_file() {
+  echo -e "<?xml-stylesheet type='text/css' href='/styles.css' ?>\n$(cat "$1")" > "$1"
+  npx svgo "$1"
+  mv "$1" "${1//preview\./}"
 }
 
 # Generate SVGs in output directory
 # If no arguments to script, build all
 if [[ $# -eq 0 ]] ; then
-  for f in $IN_DIR/*/*.ly; do
-    [ -f "$f" ] || break
-    make_svg $f
+  for f in "$IN_DIR"/*/*.ly; do
+    make_svg "$f"
   done
 
-  cd $OUT_DIR
+  cd $OUT_DIR || exit 1
   for f in *.preview.svg; do
-    [ -f "$f" ] || break
-    prep_file $f
+    prep_file "$f"
   done
 else
-  make_svg $1
-  cd $OUT_DIR
-  prep_file "$(echo $1 | grep -E -o '(\d+)').preview.svg"
+  make_svg "$1"
+  cd $OUT_DIR || exit 1
+  prep_file "$(echo "$1" | grep -E -o '(\d+)').preview.svg"
 fi
